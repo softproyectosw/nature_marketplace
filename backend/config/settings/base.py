039ad85d108092385ad9b7e860799563e74acf32
Wilmer Ropero Castaño <57710742+WRopero@users.noreply.github.com>
2026-01-11 02:45:09 -0500

    Increase throttle limits to prevent 429 during testing

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -236,8 +236,8 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle',
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/hour',
-        'user': '1000/hour',
+        'anon': '1000/hour',
+        'user': '5000/hour',
     },
     
     # Schema generation
